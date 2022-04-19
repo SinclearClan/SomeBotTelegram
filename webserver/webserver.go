d561c86813b20ca8ed06fbe0eada31261b71a958
Marc Rentschler <marc-rentschler@outlook.com>
2022-04-19 04:03:04 +0200

    Add Status and Location to Notify

--- webserver/webserver.go
+++ webserver/webserver.go
@@ -28,9 +28,16 @@ func RegisterRoutes(engine *gin.Engine, tgbot *gotgbot.Bot) {
 				"message": "No id provided",
 			})
 			return
-		} else if ctx.Query("key") == "" {
+		}
+		if ctx.Query("key") == "" {
 			ctx.JSON(400, gin.H{
 				"message": "No key provided",
+			})
+			return
+		}
+		if ctx.Query("status") == "" {
+			ctx.JSON(400, gin.H{
+				"message": "No status provided",
 			})
 			return
 		}
@@ -45,10 +52,43 @@ func RegisterRoutes(engine *gin.Engine, tgbot *gotgbot.Bot) {
 		if ctx.Query("key") == cfg.Webserver.Key {
 
 			if telegram.IsOnWhitelist(chatId) {
-				telegram.Notify(tgbot, chatId)
-				ctx.JSON(200, gin.H{
-					"message": "OK",
-				})
+				if ctx.Query("status") == "arrived" {
+					telegram.Notify(ctx.Query("status"), chatId, tgbot)
+					ctx.JSON(200, gin.H{
+						"message": "OK",
+					})
+				} else if ctx.Query("status") == "b10b14" {
+					if ctx.Query("loc_lat") == "" {
+						ctx.JSON(400, gin.H{
+							"message": "No latitude provided",
+						})
+						return
+					}
+					if ctx.Query("loc_lon") == "" {
+						ctx.JSON(400, gin.H{
+							"message": "No longitude provided",
+						})
+						return
+					}
+	
+					lat, err := strconv.ParseFloat(ctx.Query("loc_lat"), 64)
+					if err != nil {
+						panic(err)
+					}
+					lon, err := strconv.ParseFloat(ctx.Query("loc_lon"), 64)
+					if err != nil {
+						panic(err)
+					}
+	
+					err = telegram.NotifyWithLocation(ctx.Query("status"), lat, lon, chatId, tgbot)
+					if err != nil {
+						panic(err)
+					}
+	
+					ctx.JSON(200, gin.H{
+						"message": "OK",
+					})
+				}
 			} else {
 				ctx.JSON(406, gin.H{
 					"message": "User not in whitelist",
